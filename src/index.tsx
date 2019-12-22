ad91fec24cc44da296987b26779d1ac07b651394
Denis Hristov <denis.hristov@leanplum.com>
2019-12-22 17:22:13 +0200

    API key exception.

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,21 @@ import "./index.scss";
 import { GiphySearchAPIConfig } from "./api/GiphySearchAPI/interfaces";
 import { createGiphySearchAPI } from "./api/GiphySearchAPI/GiphySearchAPI";
 
+if (!process.env.REACT_APP_GIPHY_API_KEY) {
+  throw new Error(
+    'Please provide a GIPHY API key in .env under the key "REACT_APP_GIPHY_API_KEY".'
+  );
+}
+
 const IS_USING_SAFARI_USER_AGENT = /^((?!chrome|android).)*safari/i.test(
   navigator.userAgent
 );
 
 const GIPHY_CONFIG: GiphySearchAPIConfig = {
-  apiKey: process.env.REACT_APP_GIPHY_API_KEY!,
+  apiKey: process.env.REACT_APP_GIPHY_API_KEY,
   rating: "G",
   lang: "en",
+  /* Unfortunately, Safari does not yet support webp format. */
   webpOverGif: !IS_USING_SAFARI_USER_AGENT
 };
 
